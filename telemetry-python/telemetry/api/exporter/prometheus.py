72389a6dac896bbe72d4642a2135ec8ac821d454
hindog <hindog@gmail.com>
2021-02-03 17:20:27 -0800

    Change stateful flag for metrics

--- telemetry-python/telemetry/api/exporter/prometheus.py
+++ telemetry-python/telemetry/api/exporter/prometheus.py
@@ -1,6 +1,9 @@
+import logging
 import os
+from typing import Sequence
 
 from opentelemetry.exporter.prometheus import PrometheusMetricsExporter as OTPrometheusMetricsExporter
+from opentelemetry.sdk.metrics.export import ExportRecord, MetricsExportResult
 
 
 class PrometheusMetricsExporter(OTPrometheusMetricsExporter):
@@ -19,6 +22,10 @@ class PrometheusMetricsExporter(OTPrometheusMetricsExporter):
 
         start_http_server(port=int(metrics_port), addr=metrics_bind_address)
         super().__init__(prefix)
+
+    def export(self, export_records: Sequence[ExportRecord]) -> MetricsExportResult:
+        logging.info(f"Exporting {len(export_records)} records")
+        return super().export(export_records)
 
     def __str__(self):
         return f"Prometheus [bind_address={self.bind_address}, prefix={self.prefix}]"
